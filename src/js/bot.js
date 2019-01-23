b2331532708dea735dab773b83ca114bf584f042
Marc Hein <info@marc-hein.de>
2019-01-23 15:17:05 +0100

    Added logging and simplified the main file

--- src/js/bot.js
+++ src/js/bot.js
@@ -1,8 +1,29 @@
-var fs = require("fs");
+const fs = require("fs");
+const winston = require("winston");
+const { Attachment } = require("discord.js");
+const config = require("./config").config;
 
-let config = {};
 let usersOfBot = {};
 let blahajImages = {};
+
+if (!fs.existsSync(config.LOG_FOLDER)) {
+	fs.mkdirSync(config.LOG_FOLDER);
+}
+
+const logger = winston.createLogger({
+	level: "info",
+	format: winston.format.json(),
+	transports: [
+		new winston.transports.File({ filename: config.LOG_FOLDER + "error.log", level: "error" }),
+		new winston.transports.File({ filename: config.LOG_FOLDER + "combined.log" })
+	]
+});
+
+if (process.env.NODE_ENV !== "production") {
+	logger.add(new winston.transports.Console({
+		format: winston.format.simple()
+	}));
+}
 
 function includesBlahaj(message) {
 	const triggerWordsForImage = ["franz", "blahaj", "blåhaj", "hai", "shark"];
@@ -10,7 +31,7 @@ function includesBlahaj(message) {
 	triggerWordsForImage.forEach(function (word) {
 		let regex = "\\b" + word + "\\b";
 		if (message.match(regex)) {
-			console.log("message (" + message + ") includes blahaj");
+			logger.info("Received message (" + message + ") includes blahaj. Proceed.");
 			includesBlahaj = true;
 		}
 	});
@@ -20,7 +41,7 @@ function includesBlahaj(message) {
 function addUser(id) {
 	fs.readFile(config.data.USERS_FILE_PATH, "utf8", function readFileCallback(err, data) {
 		if (err) {
-			console.log(err);
+			logger.error("Error in addUser: " + err);
 		} else {
 			let jsonFile = JSON.parse(data);
 			console.log(jsonFile);
@@ -29,9 +50,9 @@ function addUser(id) {
 			let jsonString = JSON.stringify(jsonFile); //convert it back to json
 			fs.writeFile(config.data.USERS_FILE_PATH, jsonString, "utf8", function (err) {
 				if (err) {
-					console.log(err);
+					logger.error("Error in addUser - writeFile: " + err);
 				}
-				console.log("The file was saved!");
+				logger.info("User " + id + "has been added. The file was saved!");
 			}); // write it back
 		}
 	});
@@ -57,10 +78,6 @@ function loadBlahajImages() {
 	});
 }
 
-function getBlahajImages() {
-	return blahajImages;
-}
-
 function isAllowed(userId) {
 	let isAllowedUser = false;
 	usersOfBot.forEach(function (user) {
@@ -71,16 +88,41 @@ function isAllowed(userId) {
 	return isAllowedUser;
 }
 
-function setConfig(cfg) {
-	config = cfg;
-	loadAllowedUsers();
-	loadBlahajImages();
+function handleMessage(client, message) {
+	if (isAllowed(message.author.id)) {
+		let messageText = (message.content).toLowerCase();
+		let doesIncludeBlahaj = includesBlahaj(messageText);
+		if (doesIncludeBlahaj) {
+			logger.info(`Got message by authorized user ${message.author.tag} (ID: ${message.author.id})!`);
+			const attachment = new Attachment(getRandomBlahajImage());
+			message.channel.send(attachment);
+			logger.info("Sending random blahaj image!");
+		}
+		if (messageText.includes("whatever")) {
+			const attachment = new Attachment(blahajImages.whatever);
+			message.channel.send(attachment);
+		}
+		if (message.author.id === config.BOT_OWNER) {
+			if (message.content === "!add") {
+				addUser(message.author.id);
+			}
+		}
+	} else if (message.author.id === client.user.id) {
+		logger.info(`Bot ${client.user.tag} wrote message: ${message.content}`);
+	} else {
+		logger.error(`Got message by unauthorized user ${message.author.tag} (ID: ${message.author.id})!`);
+	}
 }
 
+function getRandomBlahajImage() {
+	var keys = Object.keys(blahajImages);
+	return blahajImages[keys[ keys.length * Math.random() << 0]];
+}
+
+loadAllowedUsers();
+loadBlahajImages();
+
 module.exports = {
-	includesBlahaj,
-	addUser,
-	isAllowed,
-	getBlahajImages,
-	setConfig
+	logger,
+	handleMessage
 };
--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,14 @@
 const config = require("./config").config;
-
 const Bot = require("./bot");
-Bot.setConfig(config);
-
-const { Client, Attachment } = require("discord.js");
+const { Client } = require("discord.js");
 const client = new Client();
 
 client.on("ready", () => {
-	console.log(`Logged in as ${client.user.tag}!`);
+	Bot.logger.info(`Logged in as ${client.user.tag}!`);
 });
 
 client.on("message", message => {
-	if (Bot.isAllowed(message.author.id)) {
-		let messageText = (message.content).toLowerCase();
-		console.log(messageText);
-		let doesIncludeBlahaj = Bot.includesBlahaj(messageText);
-		console.log(doesIncludeBlahaj);
-		if (doesIncludeBlahaj) {
-			console.log("Sending blahaj image");
-			const attachment = new Attachment(Bot.getBlahajImages().whatever);
-			message.channel.send(attachment);
-		}
-		if (messageText.includes("whatever")) {
-			const attachment = new Attachment(Bot.getBlahajImages().whatever);
-			message.channel.send(attachment);
-		}
-		if (message.author.id === config.BOT_OWNER) {
-			if (message.content === "!add") {
-				Bot.addUser(message.author.id);
-			}
-		}
-	}
+	Bot.handleMessage(client, message);
 });
 
 client.login(config.DISCORD_TOKEN);
